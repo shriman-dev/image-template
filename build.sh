d127794ef535d2b102f7cfe927c04c1056932666
shriman <shriman-w@proton.me>
2024-09-16 21:56:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,11 +11,7 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
+rpm-ostree kargs --append-if-missing=rd.luks.options=discard --append-if-missing=rd.udev.log_priority=3 --append-if-missing=loglevel=3 --append-if-missing=sysrq_always_enabled=1 --append-if-missing=nowatchdog --append-if-missing=amdgpu.ppfeaturemask=0xffffffff --append-if-missing=processor.ignore_ppc=1 --append-if-missing=split_lock_detect=off --append-if-missing=pci=noats
 rpm-ostree install firejail firewall-config dkms zstd dmraid dosfstools exfatprogs gpart gparted nautilus-extensions nautilus-python sushi uresourced irqbalance
 
 #### Example for enabling a System Unit File

fbd6fc9271c58a04d66924f963cbff51c989fd2a
shriman <shriman-w@proton.me>
2024-09-24 13:29:05 +0530

    Tue Sep 24 13:29:05 2024

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ detect_os() {
 install-nix() {
 useradd nix && mkdir -m 0755 /nix && chown nix /nix
 curl -Lo nix-install https://nixos.org/nix/install
+chmod 777 nix-install
 su - nix -- sh nix-install --no-daemon --yes
 cp -pr /home/nix/.local/state/nix/profiles/profile-1-link /nix/var/nix/profiles/default
 }

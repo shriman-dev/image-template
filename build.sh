8577f0cad21c8df291e15141aef0d3378243ddf3
shriman <shriman-w@proton.me>
2024-09-27 14:21:55 +0530

    Fri Sep 27 14:21:54 2024

--- build.sh
+++ build.sh
@@ -41,8 +41,6 @@ done
 debloat
 
 install-pkgs() {
-# repos
-rpm-ostree install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 # setup Copr repos
 curl -Lo /usr/bin/copr https://raw.githubusercontent.com/ublue-os/COPR-command/main/copr && \
 chmod +x /usr/bin/copr
